5247114e3f90c95e29f1ce5384ea3e8dd7d964db
Deniizz-s <deniz.shahhoseini@gmail.com>
2023-11-08 23:34:55 +0100

    change

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+// Cart.js
+import React from 'react';
+
+function Cart({ cartItems }) {
+  return (
+    <div className="cart">
+      <h2>Warenkorb</h2>
+      <ul>
+        {cartItems.map((item, index) => (
+          <li key={index}>{item.name} - ${item.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Cart;
+
+
+
+
--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,51 @@
+//import React from'react';
+//import ProductCard from "./compnents/Card.jsx";
+
+
+/*function Data() {
 const products = [
     {
-        imgUrl: "",
-        name: "",
-        price: 0,
+        imgUrl: "./assets/img-1.jpg",
+        name: "Produkt 1",
+        price: 19.99,
     },
     {
-        
-    }
-]
+        imgUrl: "./assets/img-2.jpg",
+        name: "Produkt 2",
+        price: 19.99,
+    },
+    {
+       imgUrl: "./assets/img-3.jpg",
+        name: "Produkt 3",
+        price: 29.99, 
+    },
+    {
+        imgUrl: "./assets/img-4.jpg",
+        name: "Produkt 4",
+        price: 14.99,
+    },
+    {
+        imgUrl: "./assets/img-5.jpg",
+        name: "Produkt 5",
+        price: 34.99,
+    },
+    {
+        imgUrl: "./assets/img-6.jpg",
+        name: "Produkt 6",
+        price: 22.99,
+    },
+];
+
+return (
+    <div>
+      <h2>Willkommen im Online-Shop</h2>
+      <div className="product-list">
+        {products.map((product, index) => (
+          <ProductCard key={index} {...product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Data;*/
