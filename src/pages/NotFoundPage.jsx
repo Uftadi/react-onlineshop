d03f1d9fd75380043686725161094d697e8e2edd
Uftadi <akif.gueler@dci.education>
2023-11-10 16:04:02 +0100

    finished single product page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+//photo
+import notFound from "../assets/404-pages-sej-5f3ee7ff4966b-760x400.webp";
+
 function NotFoundPage() {
-	return <div>NotFoundPage</div>;
+	return (
+		<div>
+			<h1>Page not found</h1>
+			<img src={notFound} alt="notFoundImage" />
+		</div>
+	);
 }
 
 export default NotFoundPage;
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { products } from "../data/data";
+import { useContext } from "react";
+import { CartContext } from "../conetxt/CardContext";
+// css
+import singleProduct from "../css/singleProduct.module.scss";
 
 function SingleProduct() {
 	const { id } = useParams();
 	const product = products.find((product) => product.id === Number(id));
 
 	return (
-		<div className="product-list">
-			<div key={product.id} className="product">
+		<div className={singleProduct.product}>
+			<div key={product.id} className={singleProduct.product}>
 				<img src={product.image} alt={product.name} />
 				<h2>{product.name}</h2>
+				<p>
+					<strong>Details:</strong> Lorem ipsum dolor sit amet, consectetur
+					adipisicing elit. Ipsam, error.
+				</p>
 				<p>Preis: {product.price}</p>
 				<button onClick={() => addToCart(product)}>In den Warenkorb</button>
 			</div>
